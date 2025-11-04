51a2b289c720f76dc2009bb89be6b8d09debacb3
MaksatAbdimimax <maksatprograms@gmail.com>
2025-11-04 22:48:41 +0600

    Initial commit

---
+++ app/src/main/java/com/geeks/homework1month3/MainFragment.kt
@@ -0,0 +1,58 @@
+package com.geeks.homework1month3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.geeks.homework1month3.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    private lateinit var adapter: SerialAdapter
+
+    private var listSerial = arrayListOf<SerialModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadData()
+        adapter = SerialAdapter(listSerial)
+        binding.rvSerials.adapter = adapter
+    }
+    fun loadData(){
+        listSerial.add(SerialModel(img = "https://i.pinimg.com/236x/84/a6/e0/84a6e0b28c3ef9ad78e28cfac6651533.jpg?nii=t",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://upload.wikimedia.org/wikipedia/en/5/51/Rise_of_empires_ottoman.jpg",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVukqQMfCvC_YS4BfiIv__jpHb9J2zUvaRdA&s",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://i.pinimg.com/236x/84/a6/e0/84a6e0b28c3ef9ad78e28cfac6651533.jpg?nii=t",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://upload.wikimedia.org/wikipedia/en/5/51/Rise_of_empires_ottoman.jpg",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVukqQMfCvC_YS4BfiIv__jpHb9J2zUvaRdA&s",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://i.pinimg.com/236x/84/a6/e0/84a6e0b28c3ef9ad78e28cfac6651533.jpg?nii=t",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://upload.wikimedia.org/wikipedia/en/5/51/Rise_of_empires_ottoman.jpg",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVukqQMfCvC_YS4BfiIv__jpHb9J2zUvaRdA&s",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://i.pinimg.com/236x/84/a6/e0/84a6e0b28c3ef9ad78e28cfac6651533.jpg?nii=t",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://upload.wikimedia.org/wikipedia/en/5/51/Rise_of_empires_ottoman.jpg",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+        listSerial.add(SerialModel(img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVukqQMfCvC_YS4BfiIv__jpHb9J2zUvaRdA&s",
+            name = "Chenobyl", episode = "S1.E5", date = "2019"))
+    }
+}
---
+++ app/src/main/java/com/geeks/homework1month3/SerialModel.kt
@@ -0,0 +1,8 @@
+package com.geeks.homework1month3
+
+data class SerialModel(
+    val img: String,
+    val name: String,
+    val episode: String,
+    val date: String,
+)
